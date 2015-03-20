8774ac93abdd3092cb6d183336ba1eee4f42f367
Kiran Prajapati <kprajapati@thakkertech.com>
2015-03-20 18:09:23 +0530

    Added DocBlocks in map functions files

--- geodirectory-functions/map-functions/map_functions.php
+++ geodirectory-functions/map-functions/map_functions.php
@@ -1,21 +1,49 @@
 <?php
-/*-------------------------------------------------*/
-/* All map related functions
-/*-------------------------------------------------*/
+/**
+ * All map related functions used by the plugin
+ *
+ * @since 1.0.0
+ * @package GeoDirectory
+ */
 
+/**
+ * Sets global value for map data to use in the map.
+ *
+ * @since 1.0.0
+ *
+ * @global array $map_jason An empty array.
+ */
 function  geodir_init_map_jason()
 {
     global $map_jason;
     $map_jason = array();
 }
 
+/**
+ * Sets global value for map data to use in the map.
+ *
+ * @since 1.0.0
+ *
+ * @global array $map_canvas_arr An empty array.
+ */
 function geodir_init_map_canvas_array()
 {
     global $map_canvas_arr;
     $map_canvas_arr = array();
 }
 
-/* Create marker jason for map script */
+/**
+ * Create marker json for map script
+ *
+ * @since 1.0.0
+ *
+ * @global object $wpdb WordPress database abstraction object.
+ * @global array  $map_jason Map json data
+ * @global bool   $add_post_in_marker_array Display marker for post on map when it is true.
+ * @global array  $geodir_cat_icons Array of the category icon urls.
+ *
+ * @param null|WP_Post $post WP_Post object.
+ */
 function create_marker_jason_of_posts($post)
 {
     global $wpdb, $map_jason, $add_post_in_marker_array, $geodir_cat_icons;
@@ -23,7 +51,6 @@ function create_marker_jason_of_posts($post)
     if (!empty($post) && isset($post->ID) && $post->ID > 0 && (is_main_query() || $add_post_in_marker_array) && $post->marker_json != '') {
         $srcharr = array("'", "/", "-", '"', '\\');
         $replarr = array("&prime;", "&frasl;", "&ndash;", "&ldquo;", '');
-
 
         $geodir_cat_icons = geodir_get_term_icon();
         $icon = !empty($geodir_cat_icons) && isset($geodir_cat_icons[$post->default_category]) ? $geodir_cat_icons[$post->default_category] : '';
@@ -35,7 +62,14 @@ function create_marker_jason_of_posts($post)
     }
 }
 
-/* Send jason data to script and show listing map */
+/**
+ * Send json data to script to use in listing map
+ *
+ * @since 1.0.0
+ *
+ * @global array $map_jason Map json data
+ * @global array $map_canvas_arr Array of map canvas data.
+ */
 function send_marker_jason_to_js()
 {
     global $map_jason, $map_canvas_arr;
@@ -43,7 +77,6 @@ function send_marker_jason_to_js()
     if (is_array($map_canvas_arr) && !empty($map_canvas_arr)) {
         foreach ($map_canvas_arr as $canvas => $jason) {
             if (is_array($map_jason) && !empty($map_jason)) {
-
                 $canvas_jason = $canvas . "_jason";
                 $map_canvas_arr[$canvas] = array_unique($map_jason);
                 unset($cat_content_info);
@@ -55,21 +88,52 @@ function send_marker_jason_to_js()
                     $canvas_jason = '[{"totalcount":"0"}]';
 
                 $map_canvas_jason_args = array($canvas . '_jason' => $canvas_jason);
-                $map_canvas_jason_args = apply_filters('geodir_map_canvas_jason_' . $canvas, $map_canvas_jason_args);
+                
+				/**
+				 * Filter the canvas map json data
+				 *
+				 * @since 1.0.0
+				 *
+				 * @param array $map_canvas_arr Array of map canvas json data.
+				 */
+				$map_canvas_jason_args = apply_filters('geodir_map_canvas_jason_' . $canvas, $map_canvas_jason_args);
 
                 wp_localize_script('geodir-map-widget', $canvas . '_jason_args', $map_canvas_jason_args);
             } else {
                 $canvas_jason = '[{"totalcount":"0"}]';
                 $map_canvas_jason_args = array($canvas . '_jason' => $canvas_jason);
-                $map_canvas_jason_args = apply_filters('geodir_map_canvas_jason_' . $canvas, $map_canvas_jason_args);
+                
+				/**
+				 * Filter the canvas map json data
+				 *
+				 * @since 1.0.0
+				 *
+				 * @param array $map_canvas_arr Array of map canvas json data.
+				 */
+				$map_canvas_jason_args = apply_filters('geodir_map_canvas_jason_' . $canvas, $map_canvas_jason_args);
                 wp_localize_script('geodir-map-widget', $canvas . '_jason_args', $map_canvas_jason_args);
             }
         }
-
     }
 }
 
-/* Home map Taxonomy walker */
+/**
+ * Home map Taxonomy walker
+ *
+ * @since 1.0.0
+ *
+ * @global array $cat_count No. of catregories in list.
+ * @global array $geodir_cat_icons Array of the category icon urls.
+ *
+ * @param string $cat_taxonomy	The taxonomy for which to retrieve catregories.
+ * @param int    $cat_parent Parent category to retrieve its child categories. Default 0.
+ * @param bool   $hide_empty Whether to don't display category which has no posts. Default true.
+ * @param int    $pading Css padding value to styling the category display. Default 0. 
+ * @param string $map_canvas_name Canvas name of the map.
+ * @param bool   $child_collapse Whether to display child categories collapsed.
+ * @param bool   $is_home_map If home page map then true otherwise false. Default false. 
+ * @return string|void Html content of list of categories to display on map.
+ */
 function home_map_taxonomy_walker($cat_taxonomy, $cat_parent = 0, $hide_empty = true, $pading = 0, $map_canvas_name = '', $child_collapse, $is_home_map = false)
 {
     global $cat_count, $geodir_cat_icons;
@@ -92,9 +156,9 @@ function home_map_taxonomy_walker($cat_taxonomy, $cat_parent = 0, $hide_empty = 
     $cat_terms = get_terms($cat_taxonomy, array('parent' => $cat_parent, 'exclude' => $exclude_cat_str, 'hide_empty ' => $hide_empty));
 
     $main_list_class = '';
-    //If there are terms, start displaying
+    // If there are terms, start displaying
     if (count($cat_terms) > 0) {
-        //Displaying as a list
+        // Displaying as a list
         $p = $pading * 15;
         $pading++;
 
@@ -106,15 +170,12 @@ function home_map_taxonomy_walker($cat_taxonomy, $cat_parent = 0, $hide_empty = 
             $display = !$child_collapse ? '' : 'display:none';
         }
 
-
         $out = '<ul class="treeview ' . $list_class . '" style="margin-left:' . $p . 'px;' . $display . ';">';
 
         $geodir_cat_icons = geodir_get_term_icon();
 
         foreach ($cat_terms as $cat_term):
-
             $post_type = isset($_REQUEST['post_type']) ? $_REQUEST['post_type'] : 'gd_place';
-
             $icon = !empty($geodir_cat_icons) && isset($geodir_cat_icons[$cat_term->term_id]) ? $geodir_cat_icons[$cat_term->term_id] : '';
 
             if (!in_array($cat_term->term_id, $exclude_categories)):
@@ -135,10 +196,8 @@ function home_map_taxonomy_walker($cat_taxonomy, $cat_parent = 0, $hide_empty = 
                 $out .= '<li>' . $term_check . '<label>' . ucfirst($cat_term->name) . '</label><i class="fa fa-long-arrow-down"></i>';
             endif;
 
-
-            // get sub category by recurson
+            // get sub category by recursion
             $out .= home_map_taxonomy_walker($cat_taxonomy, $cat_term->term_id, $hide_empty, $pading, $map_canvas_name, $child_collapse, $is_home_map);
-
             $out .= '</li>';
 
         endforeach;
@@ -152,6 +211,4 @@ function home_map_taxonomy_walker($cat_taxonomy, $cat_parent = 0, $hide_empty = 
     }
     return;
 }
-
-
 ?>
