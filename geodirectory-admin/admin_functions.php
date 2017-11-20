0a47c8e34631b4eee7b39ee32fad4bac339449ab
Kiran Prajapati <kprajapati@thakkertech.com>
2017-11-20 18:06:38 +0530

    New core WP 4.9 plupload JS breaks drop and upload listing images - FIXED

--- geodirectory-admin/admin_functions.php
+++ geodirectory-admin/admin_functions.php
@@ -169,6 +169,7 @@ if (!function_exists('geodir_admin_scripts')) {
             'runtimes' => 'html5,silverlight,html4',
             'browse_button' => 'plupload-browse-button', // will be adjusted per uploader
             'container' => 'plupload-upload-ui', // will be adjusted per uploader
+            'drop_element' => 'dropbox', // will be adjusted per uploader
             'file_data_name' => 'async-upload', // will be adjusted per uploader
             'multiple_queues' => true,
             'max_file_size' => geodir_max_upload_size(),
