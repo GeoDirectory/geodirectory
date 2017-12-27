d6af2805b8711b519cfd1275d8c4fb34aaf30fd6
Kiran Prajapati <kprajapati@thakkertech.com>
2017-12-27 10:49:52 +0530

    Author page is broken due to JS error - FIXED
    Signed-off-by: Kiran Prajapati <kprajapati@thakkertech.com>

--- geodirectory-functions/user_functions.php
+++ geodirectory-functions/user_functions.php
@@ -43,37 +43,29 @@ function geodir_user_favourite_listing_count($user_id=false)
     return $user_listing;
 }
 
-
-
 /**
  * User functions.
  *
  * @since 1.5.9
  * @package GeoDirectory
  */
-
-function geodir_user_show_favourites($user_id='',$output_type='select'){
+function geodir_user_show_favourites( $user_id = '', $output_type = 'select' ) {
     // My Favourites in Dashboard
-    $show_favorite_link_user_dashboard = get_option('geodir_favorite_link_user_dashboard');
-    $user_favourite = geodir_user_favourite_listing_count($user_id);
+    $show_favorite_link_user_dashboard = get_option( 'geodir_favorite_link_user_dashboard' );
+    $user_favourite = geodir_user_favourite_listing_count( $user_id );
 
-    if (!empty($show_favorite_link_user_dashboard) && !empty($user_favourite)) {
-        $favourite_links = '';
-        $post_types = geodir_get_posttypes('object');
+    if ( ! empty( $show_favorite_link_user_dashboard ) && ! empty( $user_favourite ) ) {
+        $favourite_links = $output_type == 'link' ? array() : '';
+        $post_types = geodir_get_posttypes( 'object' );
 
-        $author_link = get_author_posts_url($user_id);
-        $author_link = geodir_getlink($author_link, array('geodir_dashbord' => 'true'), false);
+        $author_link = get_author_posts_url( $user_id );
+        $author_link = geodir_getlink( $author_link, array( 'geodir_dashbord' => 'true' ), false );
 
-        foreach ($post_types as $key => $postobj) {
-            if (in_array($key, $show_favorite_link_user_dashboard) && array_key_exists($key, $user_favourite)) {
-                $name = $postobj->labels->name;
-                $post_type_link = geodir_getlink($author_link, array('stype' => $key, 'list' => 'favourite'), false);
+        foreach ( $post_types as $key => $postobj ) {
+            if ( in_array( $key, $show_favorite_link_user_dashboard ) && array_key_exists( $key, $user_favourite ) ) {
+                $name = __( $postobj->labels->name, 'geodirectory' );
+                $post_type_link = geodir_getlink( $author_link, array( 'stype' => $key, 'list' => 'favourite' ), false );
 
-                $selected = '';
-
-                if (isset($_REQUEST['list']) && $_REQUEST['list'] == 'favourite' && isset($_REQUEST['stype']) && $_REQUEST['stype'] == $key && isset($_REQUEST['geodir_dashbord'])) {
-                    $selected = 'selected="selected"';
-                }
                 /**
                  * Filter favorite listing link.
                  *
@@ -82,66 +74,54 @@ function geodir_user_show_favourites($user_id='',$output_type='select'){
                  * @param string $key Favorite listing array key.
                  * @param int $current_user->ID Current user ID.
                  */
-                $post_type_link = apply_filters('geodir_dashboard_link_favorite_listing', $post_type_link, $key, $user_id);
+                $post_type_link = apply_filters( 'geodir_dashboard_link_favorite_listing', $post_type_link, $key, $user_id );
 
-                if($output_type=='select'){
-                    $favourite_links .= '<option ' . $selected . ' value="' . $post_type_link . '">' . __(geodir_utf8_ucfirst($name), 'geodirectory') . '</option>';
-                }elseif($output_type=='link'){
-                    $favourite_links[] = '<a href="' . $post_type_link . '">' . __(geodir_utf8_ucfirst($name), 'geodirectory') . '</a>';
+                if ( $output_type == 'select' ) {
+                    $selected = '';
+                    if ( isset( $_REQUEST['list'] ) && $_REQUEST['list'] == 'favourite' && isset( $_REQUEST['stype'] ) && $_REQUEST['stype'] == $key && isset( $_REQUEST['geodir_dashbord'] ) ) {
+                        $selected = 'selected="selected"';
+                    }
+
+                    $favourite_links .= '<option ' . $selected . ' value="' . $post_type_link . '">' . $name . '</option>';
+                } elseif ( $output_type == 'link' ) {
+                    $favourite_links[] = '<a href="' . $post_type_link . '">' . $name . '</a>';
                 }
-
-
             }
         }
 
-        if ($favourite_links != '') {
-            $user = get_user_by( 'ID', $user_id );
-            if($output_type=='select') {
+        if ( ! empty( $favourite_links ) ) {
+            if ( $output_type == 'select' ) {
                 ?>
                 <li>
-                    <select id="geodir_my_favourites" class="chosen_select" onchange="window.location.href=this.value"
-                            option-autoredirect="1" name="geodir_my_favourites" option-ajaxchosen="false"
-                            data-placeholder="<?php echo esc_attr(__('My Favorites', 'geodirectory')); ?>">
-                        <option value="" disabled="disabled" selected="selected"
-                                style='display:none;'><?php echo geodir_utf8_ucfirst(esc_attr(sprintf(__("%s's Favorites", 'geodirectory'), $user->user_nicename))); ?></option>
+                    <select id="geodir_my_favourites" class="chosen_select" onchange="window.location.href=this.value" option-autoredirect="1" name="geodir_my_favourites" option-ajaxchosen="false" data-placeholder="<?php echo esc_attr__( 'My Favorites', 'geodirectory' ); ?>">
+                        <option value="" disabled="disabled" selected="selected" style='display:none;'><?php echo esc_attr( wp_sprintf( __( "%s's Favorites", 'geodirectory' ), geodir_get_client_name( $user_id ) ) ); ?></option>
                         <?php echo $favourite_links; ?>
                     </select>
                 </li>
             <?php
-            }elseif($output_type=='link'){
-                if(!empty($favourite_links)){
-                    echo implode(" | ",$favourite_links);
-                }
-
+            } elseif ( $output_type == 'link' ) {
+                echo implode( " | ", $favourite_links );
             }
         }
     }
 }
 
+function geodir_user_show_listings( $user_id = '', $output_type = 'select' ) {
+    $show_listing_link_user_dashboard = get_option( 'geodir_listing_link_user_dashboard' );
+    $user_listing = geodir_user_post_listing_count( $user_id );
 
+    if ( ! empty( $show_listing_link_user_dashboard ) && ! empty( $user_listing ) ) {
+        $listing_links = $output_type == 'link' ? array() : '';
 
-function geodir_user_show_listings($user_id='',$output_type='select'){
+        $post_types = geodir_get_posttypes( 'object' );
 
-    $show_listing_link_user_dashboard = get_option('geodir_listing_link_user_dashboard');
-    $user_listing = geodir_user_post_listing_count($user_id);
+        $author_link = get_author_posts_url( $user_id );
+        $author_link = geodir_getlink( $author_link, array( 'geodir_dashbord' => 'true' ), false );
 
-    if (!empty($show_listing_link_user_dashboard) && !empty($user_listing)) {
-        $listing_links = array();
-
-        $post_types = geodir_get_posttypes('object');
-
-        $author_link = get_author_posts_url($user_id);
-        $author_link = geodir_getlink($author_link, array('geodir_dashbord' => 'true'), false);
-
-        foreach ($post_types as $key => $postobj) {
-            if (in_array($key, $show_listing_link_user_dashboard) && array_key_exists($key, $user_listing)) {
-                $name = $postobj->labels->name;
-                $listing_link = geodir_getlink($author_link, array('stype' => $key), false);
-
-                $selected = '';
-                if (!isset($_REQUEST['list']) && isset($_REQUEST['geodir_dashbord']) && isset($_REQUEST['stype']) && $_REQUEST['stype'] == $key) {
-                    $selected = 'selected="selected"';
-                }
+        foreach ( $post_types as $key => $postobj ) {
+            if ( in_array( $key, $show_listing_link_user_dashboard ) && array_key_exists( $key, $user_listing ) ) {
+                $name = __( $postobj->labels->name, 'geodirectory' );
+                $listing_link = geodir_getlink( $author_link, array( 'stype' => $key ), false );
 
                 /**
                  * Filter my listing link.
@@ -151,38 +131,35 @@ function geodir_user_show_listings($user_id='',$output_type='select'){
                  * @param string $key My listing array key.
                  * @param int $current_user->ID Current user ID.
                  */
-                $listing_link = apply_filters('geodir_dashboard_link_my_listing', $listing_link, $key, $user_id);
-                if($output_type=='select') {
-                    $listing_links .= '<option ' . $selected . ' value="' . $listing_link . '">' . __(geodir_utf8_ucfirst($name), 'geodirectory') . '</option>';
-                }elseif($output_type=='link'){
-                    $listing_links[] = '<a href="' .$listing_link . '">' . __(geodir_utf8_ucfirst($name), 'geodirectory') . '</a>';
+                $listing_link = apply_filters( 'geodir_dashboard_link_my_listing', $listing_link, $key, $user_id );
+                if ( $output_type == 'select' ) {
+                    $selected = '';
+                    if ( ! isset( $_REQUEST['list'] ) && isset( $_REQUEST['geodir_dashbord'] ) && isset( $_REQUEST['stype'] ) && $_REQUEST['stype'] == $key ) {
+                        $selected = 'selected="selected"';
+                    }
+
+                    $listing_links .= '<option ' . $selected . ' value="' . $listing_link . '">' . $name . '</option>';
+                } elseif ( $output_type == 'link' ) {
+                    $listing_links[] = '<a href="' . $listing_link . '">' . $name . '</a>';
                 }
             }
         }
 
-        if ($listing_links != '') {
-            $user = get_user_by( 'ID', $user_id );
-            if($output_type=='select') {
+        if ( ! empty( $listing_links ) ) {
+            if ( $output_type == 'select' ) {
                 ?>
                 <li>
-                    <select id="geodir_my_listings" class="chosen_select" onchange="window.location.href=this.value"
-                            option-autoredirect="1" name="geodir_my_listings" option-ajaxchosen="false"
-                            data-placeholder="<?php echo esc_attr(__('My Listings', 'geodirectory')); ?>">
-                        <option value="" disabled="disabled" selected="selected"
-                                style='display:none;'><?php echo geodir_utf8_ucfirst(esc_attr(sprintf(__("%s's Listings", 'geodirectory'), $user->user_nicename))); ?></option>
+                    <select id="geodir_my_listings" class="chosen_select" onchange="window.location.href=this.value" option-autoredirect="1" name="geodir_my_listings" option-ajaxchosen="false" data-placeholder="<?php echo esc_attr__( 'My Listings', 'geodirectory' ); ?>">
+                        <option value="" disabled="disabled" selected="selected" style='display:none;'><?php echo esc_attr( wp_sprintf( __( "%s's Listings", 'geodirectory' ), geodir_get_client_name( $user_id ) ) ); ?></option>
                         <?php echo $listing_links; ?>
                     </select>
                 </li>
             <?php
-            }elseif($output_type=='link'){
-                if(!empty($listing_links )){
-                    echo implode(" | ",$listing_links );
-                }
-
+            } elseif ( $output_type == 'link' ) {
+                echo implode( " | ", $listing_links );
             }
         }
     }
-
 }
 
 /**
