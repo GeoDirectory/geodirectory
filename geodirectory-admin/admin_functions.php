209135b0e78fb13f413bc416a3f06ce6469b8f51
stiofan <info@wpgeodirectory.com>
2018-12-13 19:31:13 +0000

    WP Font Awesome Settings added to support CSS and JS versions - ADDED

--- geodirectory-admin/admin_functions.php
+++ geodirectory-admin/admin_functions.php
@@ -67,9 +67,9 @@ if (!function_exists('geodir_admin_styles_req')) {
     function geodir_admin_styles_req()
     {
 
-        wp_register_script('font-awesome', 'https://use.fontawesome.com/releases/v5.5.0/js/all.js#faload', array('font-awesome-shim'), GEODIRECTORY_VERSION);
-        wp_register_script('font-awesome-shim', 'https://use.fontawesome.com/releases/v5.5.0/js/v4-shims.js', array(), GEODIRECTORY_VERSION);
-        wp_enqueue_script( 'font-awesome' );
+//        wp_register_script('font-awesome', 'https://use.fontawesome.com/releases/v5.5.0/js/all.js#faload', array('font-awesome-shim'), GEODIRECTORY_VERSION);
+//        wp_register_script('font-awesome-shim', 'https://use.fontawesome.com/releases/v5.5.0/js/v4-shims.js', array(), GEODIRECTORY_VERSION);
+//        wp_enqueue_script( 'font-awesome' );
 
         wp_register_script('geodirectory-admin', geodir_plugin_url() . '/geodirectory-assets/js/admin-req.min.js', array('jquery'), GEODIRECTORY_VERSION);
         wp_enqueue_script('geodirectory-admin');
